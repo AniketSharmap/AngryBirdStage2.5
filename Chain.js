80ffeb245b07dbdf2fb00c4284a54714377095e0
AniketSharmap <73336096+AniketSharmap@users.noreply.github.com>
2020-11-21 11:34:25 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,19 @@
+class Chain {
+    constructor(BodyA, BodyB) {
+        var option =
+        {
+            bodyA: BodyA,
+            bodyB: BodyB,
+            sttifness: 0.08,
+            length: 10,
+        }
+        this.chain = Constraint.create(option);
+        World.add(world, this.chain);
+    }
+    display()
+    {
+        var point1 = this.chain.bodyA.position;
+        var point2 = this.chain.bodyB.position;
+        line(point1.x,point1.y,point2.x,point2.y);
+    }
+};
